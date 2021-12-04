ab2f354924d34ef27ecd101ee99955a6b8d25bfa
abdulrehman-10p <rehman.kaim@10pearls.com>
2021-12-04 11:43:40 +0500

    yargs module basic

--- index.js
+++ index.js
@@ -7,9 +7,32 @@ yargs.version("1.1.0");
 yargs.command({
   command: "add",
   describe: "abdul rehman",
-  handler: function () {
-    console.log(`hello adding`);
+  handler: function (argv) {
+    console.log(`hello adding`, argv);
   },
 });
 
-yargs.parse();
+yargs.command({
+  command: "subtract",
+  describe: "subtracting the value",
+  builder: {
+    title: {
+      describe: "subtractict ...",
+      demandOption: true, // making this field required
+      type: "string", // must be string
+    },
+    body: {
+      describe: "subtractict ...",
+      demandOption: true, // making this field required
+      type: "string", // must be string
+    },
+  },
+  handler: function (args) {
+    console.log(`removing time ` + args.title);
+  },
+});
+
+yargs.parse(); // print the value
+
+// command to run
+// nodemon index.js subtract --title="subtract by 2" --body="hello body"
